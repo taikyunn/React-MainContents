5c089c3f19d738c132be57883ce774154d46ee24
taikyunn <taichi_141@me.com>
2022-04-16 15:23:06 +0900

    jsxの導入
    公式ドキュメントの「Jsxの導入」セクション完了

--- jsx.js
+++ jsx.js
@@ -27,6 +27,7 @@ const element1 = (
   <h1>
     Hello, {formatName(user)}!
     {getGreeting()}
+    <a href="https://www.reactjs.org"> link </a>;
   </h1>
 )
 
@@ -37,3 +38,44 @@ ReactDOM.render(
 
 // console:Hello, Harper Perez!
 // console:Hello, Stranger.
+
+// />で閉じることが可能
+const element3 = <img src={user.avatarUrl} />;
+
+// JSXのタグは小要素を持つことができる
+const element4 = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+)
+
+// XSS対策でJSXは描画前に内部の値を全て文字列にエスケープするようになっている
+// つまりユーザーの入力したあらゆるコードが注入できないようになっている。
+const title = response.potentiallyMaliciousInput;
+// This is safe
+const element5 = <h1>{title}</h1>
+
+// element6とelement7は等価である
+const element6 = (
+  <h1 className="greeting">
+    Hello World
+  </h1>
+);
+
+const element7 = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello World'
+);
+
+
+// element7で使ったReact.createElementを詳しくみてみる
+// 別名:React要素と呼ばれる
+const element8 = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello World',
+  }
+}
