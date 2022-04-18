fc9c72436339df3507a708367397c1074525c029
taikyunn <taichi_141@me.com>
2022-04-18 10:39:31 +0900

    コンポーネントの基礎
    公式ドキュメント「コンポーネントとprops」完了

--- component.js
+++ component.js
@@ -22,3 +22,56 @@ ReactDOM.render(
   element,
   document.getElementById('root')
 )
+
+// コンポーネントの再利用
+// 新規Reactアプリは海藻の一番上に単一のAppコンポーネントを持っている。
+function App() {
+  return (
+    <div>
+      <Welcome name='Sara' />
+      <Welcome name='Cahal' />
+      <Welcome name='Edite' />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+// コンポーネントの分割
+// 全てのReactコンポーネントは自己のpropsに対して順関数のように振る舞う必要がある
+function Comment(props) {
+  return (
+    <div className="Comment">
+    <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img className='Avatar'
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+      />
+  )
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.author} />
+      <div className="UserInfo-name">
+        {props.author.name}
+      </div>
+    </div>
+  );
+}
